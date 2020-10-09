e8e5ad15969b656769f32472bff4290051d6baea
Sachin Kotwal <kotsachin@gmail.com>
2020-10-09 06:16:05 +0000

    Added better version of the fucntion and test case

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 EXTENSION = minmax        # the extensions name
 DATA = minmax--0.0.1.sql  # script files to install
-
+REGRESS = minmax_test
 # postgres build stuff
 PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
--- minmax--0.0.1.sql
+++ minmax--0.0.1.sql
@@ -1,6 +1,7 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION minmax" to load this file. \quit
 
+/*
 CREATE OR REPLACE FUNCTION _final_minmax(NUMERIC[])
    RETURNS text 
 immutable
@@ -24,6 +25,34 @@ BEGIN
      ORDER BY 1
    ) sub);
 return min || ' -> ' || max;
+END;
+$$;
+*/
+
+CREATE OR REPLACE FUNCTION _final_minmax(arr NUMERIC[])
+   RETURNS text
+immutable
+language plpgsql
+AS $$
+declare
+   min NUMERIC;
+   max NUMERIC;
+   len INT;
+BEGIN
+   len = array_length(arr, 1);
+   IF len = 0 THEN
+   	return NULL;
+   END IF;
+	min := arr[1];
+	max := arr[1];
+   FOR i IN 2..len LOOP
+   	IF (min > arr[i]) THEN
+		min := arr[i];
+	ELSIF (max < arr[i]) THEN
+		max := arr[i];
+	END IF;
+   END LOOP;
+	return min || ' -> ' || max;
 END;
 $$;
  
